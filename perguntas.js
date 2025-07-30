0eb048e8c5a97e242edd7bae8a350ed3f8a9ba14
mateus du rocki <145364645+mateusalexandr@users.noreply.github.com>
2025-07-30 08:27:01 -0300

    abao

---
+++ perguntas.js
@@ -0,0 +1,18 @@
+criaCartao (
+    'futebol',
+    'O que e impedimento?',
+    'Ocorre quando um jogador atacante está em posição mais avançada que o penúltimo jogador adversário (geralmente um zagueiro, mas pode ser qualquer jogador de linha, excluindo o goleiro) no momento em que a bola é tocada por um companheiro de equipe, e esse jogador participa do lance de alguma forma.',
+    
+)
+
+criaCartao (
+    'futebol',
+    'o que e lateral?',
+    'Pelo jogador da equipe adversária àquela que tocou a bola por último antes dela sair pela linha lateral',
+)
+
+criaCartao (
+    'futebol',
+    'o que e escanteio?',
+    'Quando a bola ultrapassa completamente a linha de gol, no chão ou no ar, tendo tocado por último em um jogador da equipe defensora, e o gol não é marcado.',
+)
